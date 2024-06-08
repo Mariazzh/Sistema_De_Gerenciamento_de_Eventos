36ecd4e73af4d003039f2fa9815ad899e9a0cd70
Josiane-Rodrigues <151976901+Josiane-Rodrigues@users.noreply.github.com>
2024-06-07 23:52:10 -0300

    Update main.js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,6 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
 const baseURL = 'http://localhost:3001';
+// Função para registrar um novo usuário
+  async function registerUser(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
+    try {
+      const response = await axios.post(${baseURL}/register, { name, email, password });
+      if (response.data) {
+        alert('Usuário registrado com sucesso!');
+        window.location.href = 'login.html';
+      }
+    } catch (error) {
+      console.error('Erro ao registrar usuário:', error);
+      alert('Erro ao registrar usuário.');
+    }
+  }
+
+  const registerForm = document.getElementById('registerForm');
 
 
 
